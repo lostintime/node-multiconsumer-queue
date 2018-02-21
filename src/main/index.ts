a33877ce335c77a830d260be542f0dbefcbc72f4
lostintime <lostintime.dev@gmail.com>
2018-02-21 18:16:00 +0200

    Add QueueTopic and ConsumerGroupId refined types

--- src/main/index.ts
+++ src/main/index.ts
@@ -28,6 +28,35 @@ export type DoneCallback = (error?: Error | null, value?: any) => void
  */
 export type ProcessCallback<Job> = (job: Job, done: DoneCallback) => void
 
+/**
+ * Queue topic name refined type
+ */
+export type QueueTopic = string & { _tag: "QueueTopic" }
+
+/**
+ * QueueTopic constructor
+ * @constructor
+ */
+export function QueueTopic(topic: string): QueueTopic {
+  return topic as QueueTopic
+}
+
+/**
+ * Consumer Group identifier refined type
+ */
+export type ConsumerGroupId = string & { _tag: "ConsumerGroupId" }
+
+/**
+ * ConsumerGroupId constructor
+ * @constructor
+ */
+export function ConsumerGroupId(groupId: string): ConsumerGroupId {
+  return groupId as ConsumerGroupId
+}
+
+/**
+ * {@Link Consumer} accepts messages
+ */
 export interface Consumer {
   /**
    * Add new Message/Task to queue
@@ -36,6 +65,9 @@ export interface Consumer {
   add(data: any): void
 }
 
+/**
+ * {@Link Producer} is a source of messages which can be consumed by attaching a callback function
+ */
 export interface Producer<Job> {
   /**
    * Attach queue processing handler with processing parallelism
@@ -45,117 +77,149 @@ export interface Producer<Job> {
   process(fn: ProcessCallback<Job>, n?: number): void
 }
 
+/**
+ * {@Link Queue} is a {@Link Consumer} and a {@Link Producer}
+ */
 export interface Queue<Job> extends Producer<Job>, Consumer {
 
 }
 
+/**
+ * A {@Link Consumer} which accepts message to different topics identified by a {@Link TopicName}
+ */
 export interface NamedConsumer {
-  add(name: string, data: any): void
-}
-
-export interface NamedProducer<Job> {
-  /**
-   * Attach queue processing handler for given topic name and parallelism
-   * @param {string} name
-   * @param {MultiConsumerQueue.ProcessCallback<Job>} fn
-   * @param {number} n
-   */
-  process(name: string, fn: ProcessCallback<Job>, n?: number): void
+  add(topic: QueueTopic, data: any): void
 }
 
 /**
- * A Queue with named elements, can be produced/consumed with/by name
+ * A {@Link Producer} which produces messages to different {@Link QueueTopic}
+ */
+export interface NamedProducer<Job> {
+  /**
+   * Attach queue processing handler for given topic name and parallelism
+   * @param {QueueTopic} topic
+   * @param {MultiConsumerQueue.ProcessCallback<Job>} fn
+   * @param {number} n
+   */
+  process(topic: QueueTopic, fn: ProcessCallback<Job>, n?: number): void
+}
+
+/**
+ * A {@Link Queue} is a {@Link NamedProducer} and a {@Link NamedConsumer}
  */
 export interface NamedQueue<Job> extends NamedProducer<Job>, NamedConsumer {
 
 }
 
 /**
- * Defines Producer than will dispatch jobs to all consumer groups
- * Interface is same with NamedProducer<Job> but name argument means groupId
+ * Defines a {@Link Producer} than can be consumed by multiple groups idenified by {@Link ConsumerGroupId}
+ *
+ * All distinct groups will process same set of messages
  */
 export interface MultiConsumerProducer<Job> {
   /**
    * Process jobs within group defined by groupId identifier
-   * @param {string} groupId consumer group identifier
+   * @param {ConsumerGroupId} groupId consumer group identifier
    * @param {MultiConsumerQueue.ProcessCallback<Job>} fn processing function
    * @param {number} n process parallelism, defaults to 1
    */
-  process(groupId: string, fn: ProcessCallback<Job>, n?: number): void
+  process(groupId: ConsumerGroupId, fn: ProcessCallback<Job>, n?: number): void
 }
 
 /**
- * Queue may be consumed by multiple groups
+ * A Queue type which may be consumed by multiple groups
+ *
+ * It also adds the ability to _unsubscribe_ specific consumer group when it is not going
+ *  to consume Queue messages anymore, to avoid storing messages
  */
 export interface MultiConsumerQueue<Job> extends Consumer, MultiConsumerProducer<Job> {
   /**
    * Removes consumer group for current topic
    * Use this for cleanup, replace process(groupId) with removeGroup(groupId), deploy for a while
    */
-  removeGroup(groupId: string): this
+  removeGroup(groupId: ConsumerGroupId): this
 }
 
 /**
- * Wraps a NamedQueue for given name, exposes Queue interface
+ * Wraps a {@Link NamedQueue} with given name, exposing {@Link Queue} interface
  */
 export class NamedQueueWrap<Job> implements Queue<Job> {
-  constructor(private readonly _name: string,
+  constructor(private readonly _topic: QueueTopic,
               private readonly _out: NamedQueue<Job>) {
   }
 
   add(data: any): void {
-    this._out.add(this._name, data)
+    this._out.add(this._topic, data)
   }
 
   process(fn: ProcessCallback<Job>, n?: number): void {
-    this._out.process(this._name, fn, n)
+    this._out.process(this._topic, fn, n)
   }
 }
 
 /**
- * Passes calls to out NamedQueue by transforming input topic name using give function
+ * Passes calls to out {@Link NamedQueue} but transforming input topic name using given map function
  */
 export class DynamicallyNamedQueue<Job> implements NamedQueue<Job> {
 
-  constructor(readonly name: (name: string) => string,
+  constructor(readonly topic: (t: QueueTopic) => QueueTopic,
               readonly out: NamedQueue<Job>) {
   }
 
-  add(name: string, data: any): void {
-    this.out.add(this.name(name), data)
+  add(topic: QueueTopic, data: any): void {
+    this.out.add(this.topic(topic), data)
   }
 
-  process(name: string, fn: ProcessCallback<Job>, n?: number): void {
-    this.out.process(this.name(name), fn, n)
+  process(topic: QueueTopic, fn: ProcessCallback<Job>, n?: number): void {
+    this.out.process(this.topic(topic), fn, n)
   }
-}
-
-export interface EventBus<Job> {
-  topic(name: string): MultiConsumerQueue<Job>
 }
 
 /**
- * MultiConsumer queue implementation
+ * EventBus is a communication channel to publish/subscribe for messages by topics
+ */
+export interface EventBus<Job> {
+  /**
+   * Returns new EventBus topic queue instance
+   */
+  topic(topic: QueueTopic): MultiConsumerQueue<Job>
+}
+
+/**
+ * {@Link MultiConsumerQueue} implementation helper
+ *
+ * Dispatches messages from source Queue to out {@Link NamedQueue} using all group IDs
+ *  (syncronized using a RedisLiveSet) as topic names
  */
 export class MultiConsumerQueueImpl<Job> implements MultiConsumerQueue<Job> {
-  private _lastGroupsSet: Set<string> = Set()
-
+  /**
+   * @param _source queue to add and dispatch messages from
+   * @param _out a named queue to attach final processors to using groupId as topic name
+   * @param _groups live set to synchronize consumer groups across nodes
+   * @param _jobDataLens lens to extract data from Job, to support multiple implementations
+   */
   constructor(private readonly _source: Queue<Job>,
               private readonly _out: NamedQueue<Job>,
               private readonly _groups: RedisLiveSet<string>,
-              private readonly _jobDataLens: (j: Job) => any) {
+              private readonly _jobDataLens: (j: Job) => any,
+              private _lastGroupsSet: Set<string> = Set()) {
+    /**
+     * Subscribe for consumer group ids changes
+     */
     this._groups.subscribe((consumers) => {
       this._lastGroupsSet = consumers
     })
 
-    // subscribe source processing only when groups initialized
+    // subscribe source processing once groups initialized
     this._groups.onMembersInit(() => {
       // group members list initialized (loaded from db), can now process source jobs
       this._source.process((job, cb) => {
-        // dispatch job data to all consumer groups
+        /**
+         * Dispatch messages from source {@Link Queue} to out {@Link NamedQueue} using ConsumerGroupId as QueueTopic
+         */
         this._lastGroupsSet.forEach((groupId) => {
           if (groupId) { // forEach interface is fixed in v4 (may not be undefined)
-            this._out.add(groupId, this._jobDataLens(job))
+            this._out.add(QueueTopic(groupId), this._jobDataLens(job))
           }
         })
 
@@ -174,22 +238,32 @@ export class MultiConsumerQueueImpl<Job> implements MultiConsumerQueue<Job> {
   }
 
   /**
-   * Add processing group
-   * @param {string} groupId
+   * Subscribe for processing using given groupId
+   *
+   * All subscribers using same groupId will split messages processing
+   * @param {ConsumerGroupId} groupId
    * @param {ProcessCallback<Job>} fn
    * @param {number} n
    */
-  process(groupId: string, fn: ProcessCallback<Job>, n?: number): void {
+  process(groupId: ConsumerGroupId, fn: ProcessCallback<Job>, n?: number): void {
+    /**
+     * Register new consumer group so sources can dispatch messages for new group
+     */
     this._groups.add(groupId)
-    this._out.process(groupId, fn, n)
+
+    /**
+     * Attach handler itself for processing
+     */
+    this._out.process(QueueTopic(groupId), fn, n)
   }
 
   /**
    * Removes consumer group for current topic
    * Use this for cleanup, replace process(groupId) with removeGroup(groupId), deploy for a while
    */
-  removeGroup(groupId: string): this {
+  removeGroup(groupId: ConsumerGroupId): this {
     this._groups.remove(groupId)
+
     return this
   }
 }
@@ -198,15 +272,22 @@ export class MultiConsumerQueueImpl<Job> implements MultiConsumerQueue<Job> {
  * EventBus implementation
  */
 export class EventBusImpl<Job> implements EventBus<Job> {
-  private _queueMap: Map<string, MultiConsumerQueue<Job>> = Map()
+  private _queueMap: Map<QueueTopic, MultiConsumerQueue<Job>> = Map()
 
-  constructor(private readonly _buildQueue: (name: string) => MultiConsumerQueue<Job>) {
+  /**
+   * @param _buildQueue MultiConsumerQueue builder function
+   */
+  constructor(private readonly _buildQueue: (t: QueueTopic) => MultiConsumerQueue<Job>) {
   }
 
-  topic(name: string): MultiConsumerQueue<Job> {
-    if (!this._queueMap.has(name)) {
-      this._queueMap = this._queueMap.set(name, this._buildQueue(name))
+  topic(topic: QueueTopic): MultiConsumerQueue<Job> {
+    /**
+     * Create queue for given topic name and store the reference
+     */
+    if (!this._queueMap.has(topic)) {
+      this._queueMap = this._queueMap.set(topic, this._buildQueue(topic))
     }
-    return this._queueMap.get(name)
+
+    return this._queueMap.get(topic)
   }
 }
